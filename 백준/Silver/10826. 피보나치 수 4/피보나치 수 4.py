64fc3277437a5798c179f07f7ff068fd7af44ae7
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2023-08-27 18:33:59 +0900

    [Silver V] Title: 피보나치 수 4, Time: 116 ms, Memory: 241112 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def main():
+    n = int(input())
+    if n == 0:
+        print(0)
+        return
+    mat = calc_mat_power([[1, 1], [1, 0]], n - 1)
+    print(mat[0][0])
+
+
+def calc_mat_power(M: list[list[int]], n: int) -> list[list[int]]:
+    if n == 0:
+        return [[1, 0], [0, 1]]
+    elif n == 1:
+        return M
+    elif n == 2:
+        return product_mat(M, M)
+    else:
+        if n % 2 == 1:
+            temp = calc_mat_power(M, (n - 1) // 2)
+            temp = product_mat(temp, temp)
+            return product_mat(temp, M)
+        else:
+            temp = calc_mat_power(M, n // 2)
+            return product_mat(temp, temp)
+
+
+def product_mat(M1: list[list[int]], M2: list[list[int]]) -> list[list[int]]:
+    a1 = M1[0][0] * M2[0][0] + M1[0][1] * M2[1][0]
+    a2 = M1[0][0] * M2[0][1] + M1[0][1] * M2[1][1]
+    a3 = M1[1][0] * M2[0][0] + M1[1][1] * M2[1][0]
+    a4 = M1[1][0] * M2[0][1] + M1[1][1] * M2[1][1]
+    return [[a1, a2], [a3, a4]]
+
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    sys.setrecursionlimit(10**5)
+    main()
