524a0fe9f094f9b02b2fd53efec85cfc9ccca9f6
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2023-08-27 18:38:02 +0900

    [Bronze V] Title: A+B - 4, Time: 136 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -0,0 +1,11 @@
+def main():
+    try:
+        while True:
+            A, B = map(int, input().split())
+            print(A + B)
+    except:
+        pass
+
+
+if __name__ == '__main__':
+    main()
