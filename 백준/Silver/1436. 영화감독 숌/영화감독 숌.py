d37c3ae4769c08607d914e27646e294e1e0d23f7
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2023-08-27 17:50:58 +0900

    [Silver V] Title: 영화감독 숌, Time: 200 ms, Memory: 114980 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,13 @@
+def main():
+    N = int(input())
+    count = 0
+    num = 0
+    while count < N:
+        num += 1
+        if '666' in str(num):
+            count += 1
+    print(num)
+
+
+if __name__ == '__main__':
+    main()
