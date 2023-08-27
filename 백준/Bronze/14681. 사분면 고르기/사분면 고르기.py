1cfb30335aa96c4a098015c813c7e91e680417d7
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2023-08-27 18:49:58 +0900

    [Bronze V] Title: 사분면 고르기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main():
+    x = int(input())
+    y = int(input())
+    if x > 0 and y > 0:
+        print(1)
+    elif x < 0 and y > 0:
+        print(2)
+    elif x < 0 and y < 0:
+        print(3)
+    else:
+        print(4)
+
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    main()
