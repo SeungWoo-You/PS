57f2569ae90b92233e1b25b31f0a08e73ebfbff8
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2023-10-26 17:22:16 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def main():
+    T = (
+        '.  .   .\n'
+        '|  | _ | _. _ ._ _  _\n'
+        '|/\|(/.|(_.(_)[ | )(/.'
+    )
+    print(T)
+
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    main()
