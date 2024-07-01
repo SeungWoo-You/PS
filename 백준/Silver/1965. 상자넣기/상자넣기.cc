9ee91c58c68bb7d89e2a2efb20e50659e395529b
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2024-07-01 16:43:39 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	vector<int> boxes(N + 1), lasts;
+	for (int i = 1; i <= N; i++) cin >> boxes[i];
+
+	lasts.push_back(0);
+	lasts.push_back(boxes[1]);
+
+	for (int i = 2; i <= N; i++) {
+		int start = 1, end = lasts.size() - 1;
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (lasts[mid] >= boxes[i]) end = mid - 1;
+			else start = mid + 1;
+		}
+
+		if (start == (int) lasts.size()) lasts.push_back(boxes[i]);
+		else lasts[start] = boxes[i];
+	}
+	
+	cout << lasts.size() - 1;
+
+	return 0;
+}
