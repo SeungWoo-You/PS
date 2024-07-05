95eb976ab9fa4611072954a3ea1e32048ea97269
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2024-07-05 11:09:17 +0900

    [Platinum III] Title: 요세푸스 문제 2, Time: 32 ms, Memory: 3588 KB -BaekjoonHub

---
+++ 백준/Platinum/1168. 요세푸스 문제 2/요세푸스 문제 2.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Josephus {
+private:
+	int N, K;
+	vector<int> cnts;
+
+	void build(int node, int s, int e, int i) {
+		if (s == e) {
+			cnts[node] = 1;
+			return;
+		}
+
+		int mid = (s + e) / 2;
+		if (i <= mid)
+			build(node << 1, s, mid, i);
+		else
+			build(node << 1 | 1, mid + 1, e, i);
+
+		cnts[node] = cnts[node << 1] + cnts[node << 1 | 1];
+	}
+
+public:
+	Josephus(int N, int K) {
+		this->N = N, this->K = K;
+		cnts = vector<int>(4 * (N + 1), 0);
+
+		for (int i = 1; i <= N; i++)
+			build(1, 1, N, i);
+	}
+
+	int find(int node, int l, int r, int k) {
+		cnts[node]--;
+		if (l == r) return l;
+
+		int mid = (l + r) / 2;
+		if (cnts[node << 1] >= k)
+			return find(node << 1, l, mid, k);
+		else
+			return find(node << 1 | 1, mid + 1, r, k - cnts[node << 1]);
+	}
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int N, K;
+	cin >> N >> K;
+
+	Josephus ST(N, K);
+	int last = 1;
+
+	cout << '<';
+	for (int i = 0; i < N; i++) {
+		int rem = N - i;
+		last += K - 1;
+
+		if (last % rem == 0) last = rem;
+		else last %= rem;
+
+		int x = ST.find(1, 1, N, last);
+		
+		if (i != N - 1) cout << x << ", ";
+		else cout << x << '>';
+	}
+
+	return 0;
+}
