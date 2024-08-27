943fc3ed182df4c76c68b3b0f4325502667a4c5c
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2024-08-27 21:08:43 +0900

    [Platinum V] Title: 사탕상자, Time: 44 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Platinum/2243. 사탕상자/사탕상자.cc
+++ 백준/Platinum/2243. 사탕상자/사탕상자.cc
@@ -1,63 +1,61 @@
 #include <iostream>
 #include <vector>
-
-#define SIZE 1000001
+#include <cmath>
 
 using namespace std;
 
 typedef long long LLT;
 
-void update(vector<LLT>* T, int node, int start, int end, int i, LLT val) {
-	if (i < start || i > end) return;
-	if (start == end) {
-		(*T)[node] = val;
-		return;
+class SegTree {
+private:
+	int K = pow(2, 20);
+	vector<LLT> T = vector<LLT>(2 * K, 0);
+
+public:
+	void update(int p, LLT delta) {
+		for (T[p += K] += delta; p > 1; p >>= 1)
+			T[p >> 1] += delta;
 	}
 
-	int mid = (start + end) / 2;
-	update(T, node << 1, start, mid, i, val);
-	update(T, node << 1 | 1, mid + 1, end, i, val);
-	(*T)[node] = (*T)[node << 1] + (*T)[node << 1 | 1];
+	int query(LLT target) {
+		int p = 1;
 
-	return;
-}
+		while (p < K) {
+			T[p]--;
+			if (T[p << 1] >= target)
+				p <<= 1;
+			else
+				target -= T[p << 1], p = p << 1 | 1;
+		}
 
-int query(vector<LLT>* T, int node, int start, int end, LLT k) {
-	if (start == end) return end;
+		T[p]--;
 
-	int mid = (start + end) / 2;
-	if ((*T)[node << 1] >= k)
-		return query(T, node << 1, start, mid, k);
-	else
-		return query(T, node << 1 | 1, mid + 1, end, k - (*T)[node << 1]);
-}
+		return p - K;
+	}
+};
 
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(NULL), cout.tie(NULL);
 
 	int N;
 	cin >> N;
-
-	vector<LLT> box(SIZE, 0), T(4 * SIZE, 0);
-
+	SegTree ST;
+	
 	while (N--) {
-		int A, B;
-		cin >> A >> B;
+		int cmd;
+		cin >> cmd;
 
-		if (A == 1) {
-			int rank = query(&T, 1, 1, SIZE, (LLT) B);
-			cout << rank << '\n';
-			update(&T, 1, 1, SIZE, rank, box[rank] - 1);
-			box[rank]--;
+		if (cmd == 1) {
+			LLT target;
+			cin >> target;
+			cout << ST.query(target) << '\n';
 		}
-		else if (A == 2) {
-			LLT C;
-			cin >> C;
-
-			update(&T, 1, 1, SIZE, B, box[B] + C);
-			box[B] += C;
+		else {
+			int p;
+			LLT delta;
+			cin >> p >> delta;
+			ST.update(p, delta);
 		}
 	}
 
