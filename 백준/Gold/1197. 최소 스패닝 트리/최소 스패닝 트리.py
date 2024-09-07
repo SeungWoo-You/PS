92e1b5d232d58b1ee80919b2e6a470c5fb9a570c
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2024-09-07 20:42:09 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 236 ms, Memory: 49372 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,30 +1,37 @@
-from collections import defaultdict
-import heapq
+import sys
 
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
-def main():
-    V, E = map(int, input().split())
-    graph: defaultdict[int, dict[int, int]] = defaultdict(dict)
-    for _ in range(E):
-        u, v, w = map(int, input().split())
-        graph[u][v] = graph[v][u] = w
-    print(find_MST(graph, V))
+V, E = map(int, input().split())
+parents: list[int] = [i for i in range(V + 1)]
 
+graph: list[tuple[int, int, int]] = []
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph.append((u, v, w));
 
-def find_MST(graph: defaultdict[int, dict[int, int]], N: int) -> int:
-    visited = [False] * (N + 1)
-    heap = [(0, 1)]
-    res = 0
-    while heap:
-        weight, node = heapq.heappop(heap)
-        if visited[node] == True:
-            continue
-        res += weight
-        visited[node] = True
-        for v in graph[node]:
-            heapq.heappush(heap, (graph[node][v], v))
-    return res
+graph.sort(key=lambda x: x[2]);
 
+def find(x: int) -> int:
+    if x == parents[x]:
+        return x
+    parents[x] = find(parents[x])
+    return parents[x]
 
-if __name__ == '__main__':
-    main()
+def union(a: int, b: int) -> None:
+    a = find(a)
+    b = find(b)
+    
+    if a < b:
+        parents[b] = a
+    else:
+        parents[a] = b
+
+answer: int = 0
+for a, b, c in graph:
+    if find(a) != find(b):
+        union(a, b)
+        answer += c
+
+print(answer)
