92ca4d051e617def070f75ea850f2a4c667e8867
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2025-04-15 20:18:41 +0900

    [Silver III] Title: 수리공 항승, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.cc
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.cc
@@ -2,48 +2,7 @@
 
 using namespace std;
 
-int sinks[1000];
-int temp_sinks[1000];
-
-int get_max(int N) {
-	int max = sinks[0];
-
-	for (int i = 1; i < N; i++) {
-		if (sinks[i] > max)
-			max = sinks[i];
-	}
-
-	return max;
-}
-
-void counting_sort(int N, int place) {
-	int counts[10] = {0};
-
-	for (int i = 0; i < N; i++) {
-		int idx = (sinks[i] / place) % 10;
-		counts[idx]++;
-	}
-
-	for (int i = 1; i < 10; i++)
-		counts[i] += counts[i - 1];
-
-	for (int i = N - 1; i >= 0; i--) {
-		int idx = (sinks[i] / place) % 10;
-		temp_sinks[counts[idx] - 1] = sinks[i];
-		counts[idx]--;
-	}
-
-	for (int i = 0; i < N; i++)
-		sinks[i] = temp_sinks[i];
-}
-
-void radix_sort(int N) {
-	int max_elem = get_max(N);
-
-	for (int place = 1; max_elem / place > 0; place *= 10)
-		counting_sort(N, place);
-}
-
+bool sinks[1001];
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -52,17 +11,17 @@ int main() {
 	int N, L;
 
 	cin >> N >> L;
-	for (int i = 0; i < N; i++)
-		cin >> sinks[i];
+	for (int i = 0, s; i < N; i++) {
+		cin >> s;
+		sinks[s] = true;
+	}
 
-	radix_sort(N);
+	int count = 0;
 
-	int count = 1, taped = sinks[0];
-
-	for (int i = 1; i < N; i++) {
-		if (sinks[i] - taped >= L) {
+	for (int i = 0; i <= 1000; i++) {
+		if (sinks[i]) {
 			count++;
-			taped = sinks[i];
+			i += L - 1;
 		}
 	}
 
