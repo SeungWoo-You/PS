83a93c82924046a64f53075a824cf21f8dac0443
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2025-05-10 20:02:49 +0900

    [Gold V] Title: 피자 굽기, Time: 56 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Gold/1756. 피자 굽기/피자 굽기.cc
+++ 백준/Gold/1756. 피자 굽기/피자 굽기.cc
@@ -1,13 +1,11 @@
 #include <iostream>
 
-#define MAX_N 300'000
 #define MAX_D 300'000
 #define INF (~0U >> 2)
 
 using namespace std;
 
 int ovens[MAX_D];
-int pizzas[MAX_N];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -26,29 +24,20 @@ int main() {
 		else if (ovens[i] < min_width) min_width = ovens[i];
 	}
 
-	for (int i = 0; i < N; i++) cin >> pizzas[i];
+	int last_pos = D - 1;
 
-	int last_pos = D, pid = 0;
-	
-	while (last_pos >= 1 && pid < N) {
-		int pizza = pizzas[pid];
-		
-		if (pizza > ovens[0]) break;
+	for (int i = 0; i < N; i++) {
+		if (last_pos < 0) break;
 
-		int start = 1, end = last_pos;
+		int pizza;
+		cin >> pizza;
 
-		while (start < end) {
-			int mid = (start + end) >> 1;
+		while (ovens[last_pos] < pizza) last_pos--;
 
-			if (ovens[mid] < pizza) end = mid;
-			else start = mid + 1;
-		}
-
-		last_pos = end - 1;
-		pid++;
+		last_pos--;
 	}
 
-	cout << (pid < N ? 0 : last_pos + 1);
+	cout << (last_pos < 0 ? 0 : last_pos + 2);
 
 	return 0;
 }
