43f8aa5ae9465eeb3129588fda9477fe16dd7073
SeungWoo-You <112685766+SeungWoo-You@users.noreply.github.com>
2025-05-24 11:40:57 +0900

    [Silver II] Title: 풍선 공장, Time: 412 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#define MAX_N 1'000'000
+
+using namespace std;
+
+int times[MAX_N];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+		cin >> times[i];
+
+	long long left = 1, right = 1e12, res = 0;
+
+	while (left <= right) {
+		long long count = 0;
+		long long mid = (left + right) >> 1;
+
+		for (int i = 0; i < N; i++)
+			count += mid / times[i];
+
+		if (count >= M) right = mid - 1, res = mid;
+		else left = mid + 1;
+	}
+
+	cout << res;
+
+	return 0;
+}
